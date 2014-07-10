e217909697e201b2022f14a6047f7d1f111119f6
Austin Warren <austin@rebuntu.(none)>
2014-07-10 16:28:37 -0400

    More playing around with ARM functionality

--- compNrun.sh
+++ compNrun.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo 'Cleaning old binary files'
+rm bin/*
 echo 'Compiling Soft_Float';
 gcc -o bin/soft_float.out src/soft_float.c
 echo 'Running Application with Input arguments babe.dat, output.dat'
@@ -8,7 +10,7 @@ gcc -o bin/soft_int.out src/soft_int.c
 echo 'Running Application with Input arguments babe.dat, output_int.dat'
 bin/soft_int.out data/babe.dat data/output_int.dat
 echo 'Compiling Application for ARM architecture'
-arm-linux-gnueabihf-gcc -o bin/soft_int_ARM.out -static src/soft_float.c
+arm-linux-gnueabi-gcc -o bin/soft_int_ARM.out -static src/soft_float.c
 echo 'Running ARM Application with Input arguments babe.dat, output_int.dat'
 qemu-arm bin/soft_int_ARM.out data/babe.dat data/output_int_ARM.dat
 
