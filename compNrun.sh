b70af922a587a233efb137d8c03ef94c568646a1
Shane Jagdis <sjagdis@rebuntu.(none)>
2014-07-23 15:37:42 -0400

    back to car again

--- compNrun.sh
+++ compNrun.sh
@@ -18,7 +18,7 @@ arm-linux-gnueabi-gcc -o bin/soft_int_ARM.out -static src/soft_int.c
 echo 'Creating assembly file for ARM_INT'
 arm-linux-gnueabi-objdump -d bin/soft_int_ARM.out > src/soft_int_arm.asm
 echo 'Running ARM Application with Input arguments babe.dat, output_int.dat'
-qemu-arm bin/soft_int_ARM.out data/babe.dat data/output_int_ARM.dat
+qemu-arm bin/soft_int_ARM.out data/car.dat data/output_int_ARM.dat
 echo 'Compiling Application float for ARM architecture'
 arm-linux-gnueabi-gcc -o bin/soft_float_ARM.out -static src/soft_float.c
 echo 'Creating assembly file for ARM_FLOAT'
