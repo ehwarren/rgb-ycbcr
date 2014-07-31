e4bf754d9ecadbaef85d19875ace63b0f7cf515c
Austin Warren <austinjwarren@gmail.com>
2014-07-30 17:44:19 -0700

    Update showimage.m

--- matlab/showimage.m
+++ matlab/showimage.m
@@ -27,19 +27,29 @@ k=1;
 for h = 1:3
     for i = 1:2800
         for j = 1:1575 
-            im_int_car(j,i,h) = uint8(tmp(k)/16);
+            im_int_car(j,i,h) = uint8(tmp(k));
             k=k+1;
         end
     end
 end
-fid = fopen('../data/output_int_ARM.dat');
-tmp = fread(fid,'int32');
+fid = fopen('../data/output_int_ARM_optimized.dat');
+tmp = fread(fid,'uint8');
 k=1;
 for h = 1:3
     for i = 1:1920
-        for j = 1:1080 
-            im_int(j,i,h) = uint8(tmp(k)/16);
-            k=k+1;
+        for j = 1:1080
+			if(h > 1)
+				if(mod(j,4)==0)
+                    k=k+1;
+                end
+                if(k > 3110400)
+                      k = 3110400;
+                end
+				im_int(j,i,h) = uint8(tmp(k));
+            else
+                im_int(j,i,h) = uint8(tmp(k));
+                k=k+1;
+            end
         end
     end
 end
